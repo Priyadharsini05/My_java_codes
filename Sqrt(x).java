790a75e524bb284c04c7e09dc6d7e06e65faeaad
Priyadharsini05 <priyadharsinim2005@gmail.com>
2025-05-17 22:33:25 +0530

    Create Sqrt(x).java
    Square root of x in java

---
+++ Sqrt(x).java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        int result = 0;
+        while(left<right){
+            int mid = left + (right - left) / 2;
+            if(mid < x/mid){
+                left = mid + 1;
+            } else{
+                right = mid;
+            }
+        }
+        return (left == x/left)?left:left-1;
+        }
+}
